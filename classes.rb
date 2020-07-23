9b08337b890e43bee8bea04dc09816193b7fceff
BMWeygant <bobbydigital108@gmail.com>
2020-07-23 15:51:55 -0400

    Added solution as well as alternate Hash syntax.

---
+++ classes.rb
@@ -0,0 +1,24 @@
+def ua_1a_class_size(class_size, new_students)
+     class_size + new_students
+end
+
+def todays_class_schedule(array)
+  array    
+end
+
+def student_quirks
+     quirks = {
+          deku: "One for All", 
+          froppy: "Frog", 
+          tsukuyomi: "Dark Shadow", 
+          creati: "Creation"}
+end
+
+#Alternate hash solution
+
+# def student_quirks
+#      quirks = {:deku => "One for All", 
+#           :froppy => "Frog", 
+#           :tsukuyomi => "Dark Shadow", 
+#           :creati => "Creation"}
+# end
